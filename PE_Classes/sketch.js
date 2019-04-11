b9c637d724cd76719fbd26623e60b9f724f49895
lhaena3 <49445423+lhaena3@users.noreply.github.com>
2019-04-10 21:46:10 -0400

    PE_Classes

---
+++ PE_Classes/sketch.js
@@ -0,0 +1,20 @@
+var myBall;
+
+function setup() {
+    createCanvas(600, 300);
+    
+    myBall = new myBall();
+  
+    console.log(myBall);
+
+}
+
+function draw() {
+  background(55, 43, 168);
+    myBall.bounce();
+    myBall.move();
+    myBall.display();
+  
+}
+
+
